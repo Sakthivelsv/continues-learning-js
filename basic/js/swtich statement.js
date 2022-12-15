af2ad996411a6f5758770e26ff8a9d3720e43753
Sakthivelsv <sakthimanoharan7211@gmail.com>
2022-12-15 16:50:39 +0530

    CL JS2

---
+++ basic/js/swtich statement.js
@@ -0,0 +1,25 @@
+days=3
+
+switch (days) {
+    case 1:
+        console.log("monday :(");
+       break;
+    case 2:
+        console.log("tuesday"); 
+       break; 
+    case 3:
+        console.log("wednesday");   
+       break;
+    case 4:
+        console.log("thurday");
+       break;
+    case 5:
+        console.log("Finally,its friday:)");
+       break;
+    case 6:
+    case 7:
+        console.log("weekend") ;
+       break;      
+    default:("there is no such days in week");
+       break;
+}
