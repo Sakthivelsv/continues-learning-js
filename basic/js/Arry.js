d9dd2d06692932962385bbc93ae831e73577ec89
Sakthivelsv <sakthimanoharan7211@gmail.com>
2022-12-16 18:14:20 +0530

    Cl js 3

---
+++ basic/js/Arry.js
@@ -0,0 +1,18 @@
+let players = ["Rohit","Virat","Rahul","Sundar"];
+
+//console.log(players);
+
+//console.log(players.length);
+
+//console.log(players[2]);
+
+//console.log(players[1]);
+
+//console.log('we have 4 best players');
+
+//let players = ["Rohit","Virat","Rahul","Sundar","Siraj","Aswin"];
+
+console.log('we have ' + players.length + ' best players');
+
+
+
