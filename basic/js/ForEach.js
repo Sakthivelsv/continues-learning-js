28f5fe98702cb6133ac96f9e9e7f26cc506ffdb3
Sakthivelsv <sakthimanoharan7211@gmail.com>
2022-12-17 17:54:45 +0530

    CL JS 5

---
+++ basic/js/ForEach.js
@@ -0,0 +1,10 @@
+let months = ['jan','feb','mar','apr','may','june','july','aug','sep','oct','nov','dec'];
+
+//months.forEach(function (month,position) {
+  //  console.log(`${position}.${month}`);
+//});
+
+
+months.forEach(function (month,position) {
+    console.log(`${position+1}.${month}`);
+});
