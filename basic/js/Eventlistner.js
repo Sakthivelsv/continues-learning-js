9604e19ff8d5ff7da84f240f952b3be49002a246
Sakthivelsv <sakthimanoharan7211@gmail.com>
2022-12-20 11:34:49 +0530

    CL JS 6.1

--- basic/js/Eventlistner.js
+++ basic/js/Eventlistner.js
@@ -1,3 +1,7 @@
-document.querySelector("button").addEventListener("click", (event) => {
-    event.target.textContent = "Added"
-})
+//document.querySelector("button").addEventListener("click", (event) => {
+  //  event.target.textContent = "Added"
+//})
+document.getElementById("btn").addEventListener("click", fun);
+function fun() {
+document.getElementById("para").innerHTML =  "Welcome to the coding word";
+}
