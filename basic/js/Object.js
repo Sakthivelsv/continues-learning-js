9a1fe8022fb576071f632122091ef1ef954a1b50
Sakthivelsv <sakthimanoharan7211@gmail.com>
2022-12-21 09:53:59 +0530

    CL JS 7

---
+++ basic/js/Object.js
@@ -0,0 +1,7 @@
+let myObject = {
+    Courses:'Html,CSS,Javascript and React',
+    Description:'From basics to advanced Learning!!',
+    language:'Tamil,English',
+}
+//console.log(myObject.language);
+console.log(`My courses is ${myObject.Courses}!! You can learn in different lang like ${myObject.language}`);
