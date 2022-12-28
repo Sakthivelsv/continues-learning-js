2653d8820e02a94d961e08f2b248094226f75525
Sakthivelsv <sakthimanoharan7211@gmail.com>
2022-12-28 17:21:19 +0530

    CL JS 14

---
+++ basic/js/InputTracking.js
@@ -0,0 +1,10 @@
+//document.querySelector('#data').addEventListener('input',(event) => {
+  //  console.log(event.target.value);
+//}) // here we get our inputs what ever we typing
+
+// Now here somechanges
+
+function search() {
+    let  myData= document.getElementById('data').value
+    console.log(myData);
+}
