f30af005c98cad180a820793a25702bcc58f3982
Sakthivelsv <sakthimanoharan7211@gmail.com>
2023-01-05 16:41:35 +0530

    CL JS 20

---
+++ basic/js/Display.js
@@ -0,0 +1,7 @@
+//let x = document.getElementById("demo");
+//demo.style.display ="block";
+// hide para to unhide
+
+// here unhide to hide
+
+let x = document.querySelector("p").style.display ="none";
