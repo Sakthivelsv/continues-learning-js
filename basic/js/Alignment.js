3a1835adec84072b561a23a8fdcc80585c99e91d
Sakthivelsv <sakthimanoharan7211@gmail.com>
2023-01-06 16:26:35 +0530

    CL JS 21

---
+++ basic/js/Alignment.js
@@ -0,0 +1,4 @@
+let x = document.getElementById("demo1").style.textAlign ='center'
+//let x = document.getElementById("demo1").style.textAlign ='right'
+//let x = document.getElementById("demo1").style.textAlign ='left'
+//let x = document.getElementById("demo1").style.textAlign ='justify'
